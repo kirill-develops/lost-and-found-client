4a8c7af75c9d53bda8331bfc051330ca0e2be28b
Kirill Tchentsov <kirill.develops@gmail.com>
2022-03-26 18:17:54 -0400

    began client google-Oauth2 process

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import HomePage from './pages/Homepage/Homepage';
+import ProfilePage from './pages/ProfilePage/ProfilePage'
+import './styles/App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/profile" exact component={ProfilePage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
