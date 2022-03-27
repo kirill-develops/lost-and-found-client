207d82ed875400c6a366ee9a7be3b2cc016f0dc3
Kirill Tchentsov <kirill.develops@gmail.com>
2022-03-26 21:39:36 -0400

    created login button for google

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,8 +1,11 @@
+import LoginButton from "../../components/LoginButton/LoginButton";
+
 const HomePage = () => {
   return (
     <section>
       <h1>Welcome to Lost&FOUND!</h1>
       <p>Your go to source for all things dev related.</p>
+      <LoginButton />
     </section>
   );
 };
