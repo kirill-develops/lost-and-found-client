eb32d2c304fa7ae595b2795decacaaad724a13a8
Kirill Tchentsov <kirill.develops@gmail.com>
2022-03-30 22:18:48 -0400

    homepage checks to see if user is already logged in

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,16 +1,45 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import axios from "axios";
+import React, { Component } from "react";
 import LoginButton from "../../components/LoginButton/LoginButton";
 import LogoutButton from "../../components/LogoutButton/LogoutButton";
 
-const HomePage = () => {
-  return (
-    <section>
-      <h1>Welcome to Lost&FOUND!</h1>
-      <p>Your go to source for all things dev related.</p>
-      <LoginButton />
-      <LogoutButton />
-    </section>
-  );
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
+
+class HomePage extends Component {
+  state = {
+    isAuthenticating: true,
+    isLoggedIn: false,
+  }
+
+  componentDidMount() {
+    axios
+      .get(`${SERVER_URL}/auth/profile`,
+        { withCredentials: true })
+      .then(_res => {
+        console.log("🚀 ~ file: Homepage.js ~ line 18 ~ HomePage ~ componentDidMount ~ _res", _res)
+
+        this.props.history.push('/dashboard');
+      })
+      .catch(_err => {
+        this.setState({ isAuthenticating: false });
+      })
+  }
+
+  render() {
+
+    if (this.state.isAuthenticating) return null;
+
+    return (
+      <section>
+        <h1>Welcome to Lost&FOUND!</h1>
+        <p>Your go to source for all things dev related.</p>
+        <LoginButton />
+        <LogoutButton />
+      </section>
+    );
+  }
 };
 
 export default HomePage;
