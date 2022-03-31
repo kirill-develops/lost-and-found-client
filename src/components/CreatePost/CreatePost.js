63b4d4ba56ca3d1b767950f2a14372b696f444c1
Kirill Tchentsov <kirill.develops@gmail.com>
2022-03-30 22:09:06 -0400

    updated CreatePost Component

--- src/components/CreatePost/CreatePost.js
+++ src/components/CreatePost/CreatePost.js
@@ -2,13 +2,23 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import LoginButton from '../LoginButton/LoginButton';
+import './CreatePost.scss';
 
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 class CreatePost extends Component {
   state = {
-    isLoggedIn: false
+    isLoggedIn: false,
+    category: "",
+    volunteer: false,
   }
+
+  // Create a change handler for all inputs
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
   isProfileComplete = (user) => {
     if (!user.city || !user.phone || !user.province || !user.volunteer) {
@@ -17,16 +27,48 @@ class CreatePost extends Component {
     return true;
   }
 
+  handleFormSubmit = e => {
+    e.preventDefault();
+    const { title, description, category } = e.target;
+
+    // Post to API (remember to use `withCredentials`)
+    axios
+      .post(
+        `${SERVER_URL}/post`,
+        {
+          title: title.value,
+          description: description.value,
+          category: category.value,
+          offer: this.state.volunteer,
+        },
+        { withCredentials: true }
+      )
+      .then(() => {
+        // Re-fetch the posts
+        this.props.onPostCreate();
+        e.target.reset();
+      })
+      .catch(err => {
+        console.log('Error creating a new post:', err);
+      })
+  }
+
   componentDidMount() {
     // Check if user is currently logged in, so we can display a form or login button conditionally
     axios
       .get(`${SERVER_URL}/auth/profile`, { withCredentials: true })
       .then(res => {
         if (res.data && this.isProfileComplete(res.data)) {
-          console.log(res.data);
-          this.setState({
-            isLoggedIn: true
-          });
+          res.data.volunteer.toLowerCase() === 'true' ?
+            this.setState({
+              isLoggedIn: true,
+              volunteer: true
+            })
+            : this.setState({
+              isLoggedIn: true,
+              volunteer: false
+            })
+
         } else if (!this.isProfileComplete(res.data)) {
           this.props.history.push('/profile');
         }
@@ -40,18 +82,30 @@ class CreatePost extends Component {
           this.state.isLoggedIn ? (
             // If user is logged in, render form for creating a post
             <form className="post-form" onSubmit={this.handleFormSubmit}>
-              <h3>Create New Post</h3>
+              {this.state.volunteer ? <h3>Create New Offer</h3> : <h3>What Can We Connect You With?</h3>}
               <div className="post-form__fields">
                 <div className="post-form__field">
                   <label
-                    htmlFor="postTitle"
+                    htmlFor="title"
                     className="post-form__label">
-                    Post Title</label>
+                    Brief Title</label>
                   <input
                     type="text"
-                    name="postTitle"
-                    id="postTitle"
+                    name="title"
+                    id="title"
                     maxLength="75"
+                    required
+                  />
+                </div>
+                <div className="post-form__field">
+                  <label
+                    htmlFor="description"
+                    className="post-form__label">
+                    Description</label>
+                  <textarea
+                    type="text"
+                    name="description"
+                    id="description"
                     required
                   />
                 </div>
@@ -59,13 +113,20 @@ class CreatePost extends Component {
                   <label
                     htmlFor="postContent"
                     className="post-form__label">
-                    Post Content</label>
-                  <textarea
-                    type="text"
-                    name="postContent"
-                    id="postContent"
-                    required
-                  />
+                    Category</label>
+                  <select
+                    name='category'
+                    value={this.state.category}
+                    onChange={this.handleChange}>
+                    <option>Please Select</option>
+                    <option value='housing'>Housing</option>
+                    <option value='jobs'>Jobs</option>
+                    <option value='employment Services'>Employment Services</option>
+                    <option value='on-boarding'>On-boarding</option>
+                    <option value='translations'>Translations</option>
+                    <option value='goods'>Free Goods</option>
+                    <option value='transportation'>Transportation</option>
+                  </select>
                 </div>
               </div>
               <button
@@ -82,31 +143,6 @@ class CreatePost extends Component {
         }
       </section>
     );
-  }
-
-  handleFormSubmit = e => {
-    e.preventDefault();
-
-    const { postTitle, postContent } = e.target;
-
-    // Post to API (remember to use `withCredentials`)
-    axios
-      .post(
-        `${SERVER_URL}/post`,
-        {
-          title: postTitle.value,
-          content: postContent.value
-        },
-        { withCredentials: true }
-      )
-      .then(() => {
-        // Re-fetch the posts
-        this.props.onPostCreate();
-        e.target.reset();
-      })
-      .catch(err => {
-        console.log('Error creating a new post:', err);
-      })
   }
 }
 
