6dba7ad345b9be3e1d02e48b143916e02bd3bfea
Kirill Tchentsov <kirill.develops@gmail.com>
2022-04-23 13:12:22 -0400

    refactored dashboardNav Logic using a custom MediaQuerryHook

--- src/components/DashboardNav/DashBoardNav.jsx
+++ src/components/DashboardNav/DashBoardNav.jsx
@@ -1,125 +1,19 @@
-/* eslint react/prop-types: 0 */
-import React, { useState } from 'react';
-import './DashBoardNav.scss';
+/* eslint-disable sort-imports */
+import './DashboardNav.scss';
+import React from 'react';
+import DashboardMobileNav from './ForMobile/ForMobile';
+import DashboardTabletNav from './ForTablet/ForTablet';
+import useMediaQuery from '../../utils/useMediaQuery';
 
-const DashBoardNav = ({ setFilterBy }) => {
-  const [active, setActive] = useState(false);
-
-
-  // on mobile aspect ratio 
-  const expandNav = () => {
-    setActive(!active);
-  }
-
-  const handleClick = (category) => {
-    setActive(false);
-    setFilterBy(category);
-  }
+const DashboardNav = ({ setFilterBy }) => {
+  const isDashNavMobile = useMediaQuery('(max-width:73.75rem)');
 
   return (
     <div className="dashboard-nav">
-
-      <div
-        className="dashboard-nav__mobile"
-      >
-        <h3
-          onClick={() => expandNav()}
-          className="dashboard-nav__label--first-mobile"
-        >
-          Filter By:
-        </h3>
-        <ul className={active ? 'dashboard-nav__mobile-block--active' : 'dashboard-nav__mobile-block'}>
-          <li
-            onClick={() => handleClick('housing')}
-            className="dashboard-nav__label"
-          >
-            Housing</li>
-          <li
-            onClick={() => handleClick('jobs')}
-            className="dashboard-nav__label"
-          >
-            Jobs</li>
-          <li
-            onClick={() => handleClick('employment_services')}
-            className="dashboard-nav__label"
-          >
-            Employment Services</li>
-          <li
-            onClick={() => handleClick('on-boarding')}
-            className="dashboard-nav__label"
-          >
-            On-Boarding</li>
-          <li
-            onClick={() => handleClick('translations')}
-            className="dashboard-nav__label"
-          >
-            Translations</li>
-          <li
-            onClick={() => handleClick('goods')}
-            className="dashboard-nav__label"
-          >
-            Free Goods</li>
-          <li
-            onClick={() => handleClick('transportation')}
-            className="dashboard-nav__label"
-          >
-            Transportation</li>
-          <li
-            onClick={() => handleClick('')}
-            className="dashboard-nav__label--reset"
-          >
-            Reset</li>
-        </ul>
-      </div>
-
-      <ul className="dashboard-nav__block">
-        <li
-          className="dashboard-nav__label--first"
-        >
-          Filter By:</li>
-        <li
-          onClick={() => setFilterBy('housing')}
-          className="dashboard-nav__label"
-        >
-          Housing</li>
-        <li
-          onClick={() => setFilterBy('jobs')}
-          className="dashboard-nav__label"
-        >
-          Jobs</li>
-        <li
-          onClick={() => setFilterBy('employment_services')}
-          className="dashboard-nav__label"
-        >
-          Employment Services</li>
-        <li
-          onClick={() => setFilterBy('on-boarding')}
-          className="dashboard-nav__label"
-        >
-          On-Boarding</li>
-        <li
-          onClick={() => setFilterBy('translations')}
-          className="dashboard-nav__label"
-        >
-          Translations</li>
-        <li
-          onClick={() => setFilterBy('goods')}
-          className="dashboard-nav__label"
-        >
-          Free Goods</li>
-        <li
-          onClick={() => setFilterBy('transportation')}
-          className="dashboard-nav__label"
-        >
-          Transportation</li>
-        <li
-          onClick={() => setFilterBy('')}
-          className="dashboard-nav__label--reset"
-        >
-          Reset</li>
-      </ul>
+      {isDashNavMobile && <DashboardMobileNav setFilterBy={setFilterBy} />}
+      {!isDashNavMobile && <DashboardTabletNav setFilterBy={setFilterBy} />}
     </div>
-  )
+  );
 };
 
-export default DashBoardNav;
+export default React.memo(DashboardNav);
---
+++ src/components/DashboardNav/ForMobile/ForMobile.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+
+const DashboardMobileNav = ({ setFilterBy }) => {
+  const [active, setActive] = useState(false);
+
+  const selectMobileFilter = (category) => {
+    setActive(false);
+    setFilterBy(category);
+  };
+
+  return (
+    <>
+      <button
+        type="button"
+        onClick={() => setActive(!active)}
+        className="dashboard-nav__label--first-mobile"
+      >
+        SELECT FILTER
+      </button>
+      {active && (
+        <nav className="dashboard-nav__active">
+          <button
+            type="button"
+            onClick={() => selectMobileFilter('housing')}
+            className="dashboard-nav__label"
+          >
+            Housing
+          </button>
+          <button
+            type="button"
+            onClick={() => selectMobileFilter('jobs')}
+            className="dashboard-nav__label"
+          >
+            Jobs
+          </button>
+          <button
+            type="button"
+            onClick={() => selectMobileFilter('housing')}
+            className="dashboard-nav__label"
+          >
+            Employment Services
+          </button>
+          <button
+            type="button"
+            onClick={() => selectMobileFilter('housing')}
+            className="dashboard-nav__label"
+          >
+            On-Boarding
+          </button>
+          <button
+            type="button"
+            onClick={() => selectMobileFilter('housing')}
+            className="dashboard-nav__label"
+          >
+            Translations
+          </button>
+          <button
+            type="button"
+            onClick={() => selectMobileFilter('housing')}
+            className="dashboard-nav__label"
+          >
+            Free Goods
+          </button>
+          <button
+            type="button"
+            onClick={() => selectMobileFilter('housing')}
+            className="dashboard-nav__label"
+          >
+            Transportation
+          </button>
+          <button
+            type="button"
+            onClick={() => selectMobileFilter('housing')}
+            className="dashboard-nav__label--reset"
+          >
+            Reset
+          </button>
+        </nav>
+      )}
+    </>
+  );
+};
+
+export default React.memo(DashboardMobileNav);
